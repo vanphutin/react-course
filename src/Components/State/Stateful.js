d3b6e351c1181292a413703e9b2506085a830234
vanphutin2004 <vanphutin.contact@gmail.com>
2024-06-24 14:47:34 +0700

    State

--- src/Components/State/Stateful.js
+++ src/Components/State/Stateful.js
@@ -6,14 +6,15 @@ const Stateful = () => {
       <h3>
         <i>Stateful Components Stateful components </i>
       </h3>
-      (còn gọi là class components hoặc stateful functional components với
-      hooks) là các thành phần có thể quản lý trạng thái nội bộ. Chúng sử dụng
-      state để lưu trữ và cập nhật dữ liệu, và có thể thay đổi giao diện dựa
-      trên trạng thái đó. Đặc điểm của Stateful Components: Quản lý trạng thái
-      nội bộ: Sử dụng state để lưu trữ dữ liệu và cập nhật giao diện khi trạng
-      thái thay đổi. Có thể phức tạp hơn: Vì phải quản lý trạng thái và logic
-      cập nhật trạng thái. Có thể sử dụng lifecycle methods (nếu là class
-      components): Các phương thức này cho phép quản lý vòng đời của thành phần.
+      (còn gọi là <strong> class components</strong> hoặc stateful functional
+      components với hooks) là các thành phần có thể quản lý trạng thái nội bộ.
+      Chúng sử dụng state để lưu trữ và cập nhật dữ liệu, và có thể thay đổi
+      giao diện dựa trên trạng thái đó. Đặc điểm của Stateful Components: Quản
+      lý trạng thái nội bộ: Sử dụng state để lưu trữ dữ liệu và cập nhật giao
+      diện khi trạng thái thay đổi. Có thể phức tạp hơn: Vì phải quản lý trạng
+      thái và logic cập nhật trạng thái. Có thể sử dụng lifecycle methods (nếu
+      là class components): Các phương thức này cho phép quản lý vòng đời của
+      thành phần.
     </div>
   );
 };
