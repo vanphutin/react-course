b5612aa13c703130d2aaca3b5d5f25276fcad22f
vanphutin2004 <vanphutin.contact@gmail.com>
2024-06-24 14:44:00 +0700

    Form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -33,7 +33,7 @@ const Form = () => {
             aria-describedby="emailHelp"
             onChange={hanldeInput}
           />
-          <div id="emailHelp" class="form-text">
+          <div id="emailHelp" class="form-text" style={{ color: "red" }}>
             We'll never share your email with anyone else.
           </div>
         </div>
