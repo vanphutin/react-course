e161216de67e3b3e6c112cde75972e0719e0c402
vanphutin2004 <vanphutin.contact@gmail.com>
2024-06-24 14:43:05 +0700

    Advanced

--- src/Components/Advanced/Advanced.js
+++ src/Components/Advanced/Advanced.js
@@ -6,10 +6,10 @@ const Advanced = () => {
     <div>
       <h1>Higher-Order Components</h1>
       <p>
-        Higher-order components(HOC) là advanced concept trong React. HOCs tương
-        đương với higher-order functions. Các bạn cứ hiểu là nó nhận đầu vào là
-        một component như là argument và trả về "phiên bản mở rộng" của
-        component đó.
+        <strong>Higher-order components(HOC) </strong> là advanced concept trong
+        React. HOCs tương đương với higher-order functions. Các bạn cứ hiểu là
+        nó nhận đầu vào là một component như là argument và trả về "phiên bản mở
+        rộng" của component đó.
       </p>
       <FetchingData />
     </div>
